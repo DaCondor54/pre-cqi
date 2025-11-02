5e329a8c0c5b6b3362ac6106e6cc1507cc6ce525
Aezurly <alice.vergeau@gmail.com>
2025-11-02 12:33:31 -0500

    target campfire when no flame in safe_distance_threshold

--- main.py
+++ main.py
@@ -33,8 +33,6 @@ def on_turn(turn_data: dict):
 # choose where to go (escape or go to powerup)
 def handle_move(turn_data: GameState, response: PlayerCommand):
     # Implement your move handling logic here
-    response.move.direction = 180
-    response.move.speed = 1
     pass
 
 # choose target (enemy or door)
@@ -43,15 +41,34 @@ def choose_target(turn_data: GameState, response: PlayerCommand) -> Flame:
     flames = turn_data.flames
     if( not flames ):
         return get_default_target(turn_data)
+    return get_flame_target(turn_data)
+
+def get_flame_target(turn_data: GameState) -> Flame:
+    player_position = turn_data.player.position
+    flames = turn_data.flames
     closest_flames = sort_flames_by_distance(player_position, flames)
+    safe_distance_threshold = closest_flames[0].hp * closest_flames[0].speed * 5
     for flame in closest_flames:
-        if (not is_flame_dying_soon(flame, turn_data.bullets)):
+        if (euclidean_distance(player_position, flame.position) < safe_distance_threshold):
+            safe_distance_threshold += flame.hp * flame.speed * 5
+            
+    if(euclidean_distance(player_position, closest_flames[0].position) < safe_distance_threshold):
+        print("Chose closest flame")
+        return closest_flames[0]
+    return get_closest_campfire(closest_flames) or closest_flames[0] or get_default_target(turn_data)
+
+def get_closest_campfire(closest_flames: list[Flame]) -> Flame | None:
+    for flame in closest_flames:
+        if flame.type == 'campfire':
             return flame
-    return closest_flames[0]
+    return None
 
 def get_closest_flame(position: Position, flames: list[Flame]) -> Flame:
-    closest_flame = min(flames, key=lambda f: euclidean_distance(position, f.position))
-    return closest_flame
+    closest_flames = sort_flames_by_distance(position, flames)
+    for flame in closest_flames:
+        if (not is_flame_dying_soon(flame, [])):
+            return flame
+    return closest_flames[0]
 
 def sort_flames_by_distance(player_position: Position, flames: list[Flame]) -> list[Flame]:
     return sorted(flames, key=lambda f: euclidean_distance(player_position, f.position))
@@ -70,6 +87,7 @@ def get_closest_flame(player_position: Position, flames: list[Flame]) -> Flame:
     return closest_flame
 
 def get_default_target(turn_data: GameState) -> Flame:
+    print("Chose default target", turn_data)
     spawn = turn_data.map.spawnPoints[0]
     spawn_position = Position(x=spawn.x + spawn.width / 2, y=spawn.y + spawn.height / 2)
     return Flame(position=spawn_position, id="", hp=0, velocity=Velocity(x=0,y=0), angle=0, speed=0, type='flame')
